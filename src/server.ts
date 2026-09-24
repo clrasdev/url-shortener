e3fbc2e6868e6df09b21f2f05270e801f64fb2bf
Maria Clara <unrotly@gmail.com>
2026-09-24 00:14:13 -0300

    feat: add URL creation endpoint

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,16 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "URL Shortener API funcionado!",
+    message: "URL Shortener API funcionando!",
+  });
+});
+
+app.post("/urls", (req, res) => {
+  const { url } = req.body;
+
+  res.json({
+    originalUrl: url,
+    message: "URL recebida com sucesso!",
   });
 });
 
